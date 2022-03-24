f5af131baa0ef04207016fedd91a01c6ca513010
Tiago Nunes de Cerqueira <83711090+TigoNunes@users.noreply.github.com>
2022-03-24 17:34:45 -0300

    ablçaba

---
+++ Codigos/Main.java
@@ -0,0 +1,39 @@
+package Codigos;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * contasBancarias
+ */
+class contasBancarias {
+
+    String idConta;
+    String nomePessoa;
+    String cpf;
+    String cidade;
+    int transefernciasRealizadas;
+    float saldoConta;
+
+    public contasBancarias(){
+        idConta = "";
+        nomePessoa = "";
+        cpf = "";
+        cidade = "";
+        transefernciasRealizadas = 0;
+        saldoConta = 0;
+    }
+
+
+
+
+
+}
+
+public class Main{
+
+    
+    public static void main(String[] args) {
+        
+    }
+}
