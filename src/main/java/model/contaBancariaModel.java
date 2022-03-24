b5ac40a89dc8c0e3d41170f03a0ac2e525b376de
Tiago Nunes de Cerqueira <83711090+TigoNunes@users.noreply.github.com>
2022-03-24 20:11:01 -0300

    fiz as paradas ai

--- src/main/java/model/contaBancariaModel.java
+++ src/main/java/model/contaBancariaModel.java
@@ -17,5 +17,64 @@ public class contaBancariaModel {
         saldoConta = 0;
     }
 
-    //criar getters and setters 
+    // Setters.......................//
+
+    public void setIdConta(String id){
+        this.idConta = id;
+    }
+
+    public void setNomePessoa(String nome){
+        this.nomePessoa = nome;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public void setCidade(String cidade){
+        this.cidade = cidade;
+    }
+
+    private void setTranferenciasRealizada(int transferencias){
+        this.transefernciasRealizadas = transferencias;
+    }
+
+    private void setSaldo(float saldo){
+        this.saldoConta = saldo;
+    }
+
+    //getters........................//
+
+    public String getIdConta(){
+        return idConta;
+    }
+
+    public String getNomePessoa(){
+        return nomePessoa;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    public String getCidade(){
+        return cidade;
+    }
+
+    public int getTranferenciasRealizada(){
+        return transefernciasRealizadas;
+    }
+
+    public float getSaldo(){
+        return saldoConta;
+    }
+
+    public void novaConta(String id, String nome, String cpf,String cidade){
+        this.idConta = id;
+        this.nomePessoa = nome;
+        this.cpf = cpf;
+        this.cidade = cidade;
+    }
+
+     
 }
